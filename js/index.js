83ebfcb8b473f71125661c173c92d302d49bb4f5
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2026-08-18 06:02:52 -0400

    conditional issues

--- js/index.js
+++ js/index.js
@@ -119,8 +119,7 @@ const getVerified = async () => {
       },
     });
     const users = await response.json();
-    greeting.innerHTML =
-      guestId && `Hi, ${users.find((user) => user._id === guestId).username}`;
+    greeting.innerHTML = `Hi, ${guestId && users.find((user) => user._id === guestId).username}`;
     if (email) {
       const foundUser = users.find((user) => user.email === email);
       if (foundUser) {

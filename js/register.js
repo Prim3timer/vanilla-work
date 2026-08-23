c4e15b86b917b7ec4af550a5a2ff08e4caa636ed
Prim3timer <dikeekwelie@gmail.com>
2026-08-23 15:22:38 -0400

    s

--- js/register.js
+++ js/register.js
@@ -64,7 +64,7 @@ const createUserDets = async () => {
       name: userNameInput.value.trim(),
       email: emailInput.value.trim().toLowerCase(),
       biz,
-      link: `https://${window.location.host}/index.html?email=${emailInput.value.trim().toLowerCase()}&elapsed=${now}`,
+      link: `http://${window.location.host}/index.html?email=${emailInput.value.trim().toLowerCase()}&elapsed=${now}`,
     };
 
     const mailSent = await emailjs.send(
